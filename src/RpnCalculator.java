883beff74ab3294d9dd8eab89222b73ccafc4e1a
jacobOrtega <jenku18@gmail.com>
2016-12-06 01:22:38 +0200

    accumulatorIsEmpty test passed

--- src/RpnCalculator.java
+++ src/RpnCalculator.java
@@ -1,4 +1,15 @@
 
-public class RpnCalculator {
 
+public class RpnCalculator {
+	
+	
+	int accumulator = 0;
+
+	public int getAccumulator() {
+		return accumulator;
+	}
+
+	public void setAccumulator(int accumulator) {
+		this.accumulator = accumulator;
+	}
 }
--- src/RpnCalculatorTests.java
+++ src/RpnCalculatorTests.java
@@ -1,4 +1,15 @@
+import org.junit.Test;
 
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+import java.util.*;
 public class RpnCalculatorTests {
 
+	RpnCalculator calc = new RpnCalculator();
+
+	@Test 
+	public void accumulatorIsEmpty(){
+		
+		assertThat(calc.getAccumulator(), is(0));
+	}
 }
